f4776e03c2d954169f889de430a101db952211a8
Danny Willems <contact@danny-willems.be>
2016-06-17 20:11:54 +0200

    Use functions instead of objects

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -1,28 +1,31 @@
 let on_device_ready () =
-  let sim = Cordova_sim_card.t () in
-  sim#request_read_permission ();
   let on_info_succ result =
     Jsoo_lib.alert
-      (result#carrier_name ^ "\n " ^
-      result#country_code ^ "\n " ^
-      result#mcc ^ "\n " ^
-      result#mnc ^ "\n " ^
-      result#phone_number ^ "\n " ^
-      result#device_id ^ "\n " ^
-      result#device_software_version ^ "\n " ^
-      result#sim_serial_number ^ "\n " ^
-      result#subscriber_id)
-      (*result#call_state ^ " " ^*)
-      (*result#data_activity ^ " " ^*)
-      (*result#network_type ^ " " ^*)
-      (*result#phone_type ^ " " ^*)
-      (*result#sim_state ^ " " ^*)
-      (*result#is_network_roaming ^ " " ^*)
+    (
+      (Cordova_sim_card.carrier_name result) ^ "\n " ^
+      (Cordova_sim_card.country_code result) ^ "\n " ^
+      (Cordova_sim_card.mcc result) ^ "\n " ^
+      (Cordova_sim_card.mnc result) ^ "\n " ^
+      (Cordova_sim_card.phone_number result) ^ "\n " ^
+      (Cordova_sim_card.device_id result) ^ "\n " ^
+      (Cordova_sim_card.device_software_version result) ^ "\n " ^
+      (Cordova_sim_card.sim_serial_number result) ^ "\n " ^
+      (Cordova_sim_card.subscriber_id result)
+      (*
+      (Cordova_sim_card.call_state result) ^ "\n" ^
+      (Cordova_sim_card.data_activity result) ^ "\n" ^
+      (Cordova_sim_card.network_type result) ^ " " ^
+      (Cordova_sim_card.phone_type result) ^ "\n" ^
+      (Cordova_sim_card.sim_state result) ^ "\n" ^
+      (Cordova_sim_card.is_network_roaming result);
+      *)
+    )
   in
   let on_info_err r =
     Jsoo_lib.alert r
   in
-  sim#info on_info_succ on_info_err
+  Cordova_sim_card.request_read_permission ();
+  Cordova_sim_card.info on_info_succ on_info_err
 
 let _ =
   Cordova.Event.device_ready on_device_ready
